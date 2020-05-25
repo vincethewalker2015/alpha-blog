27a5e19f1e26a876efb5ed0d644012a2eecca9f4
Vincent <vincethewalker@yahoo.co.uk>
2020-05-25 15:04:42 +0100

    Added Articles Model, Controller and Views

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'help', to: 'pages#help'
+  resources :articles, only: [:show, :index]
   
 end
