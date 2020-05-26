ed67e640a090da39657fbc84dfc581e3236427a9
Vincent <vincethewalker@yahoo.co.uk>
2020-05-26 15:56:27 +0100

    Added Destroy method as well as links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -20,15 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+   
   end
 
   def edit
-    @article = Article.find(params[:id])
+    
   end
 
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Article has been updated"
       redirect_to article_path(@article)
@@ -38,12 +38,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    
+    @article.destroy
+    flash[:notice] = "Article has been deleted"
+    redirect_to articles_path
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
 end
