2239bfecc4781a88d79c3e42885c0296df18201a
Vincent <vincethewalker@yahoo.co.uk>
2020-06-12 18:24:45 +0100

    Created Admin user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:notice] = "You can only Edit or Delete your own Article"
       redirect_to @article
     end
