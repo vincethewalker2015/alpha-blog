e077b8de0ed837640ae1be537551893b52c62945
Vincent <vincethewalker@yahoo.co.uk>
2020-06-29 19:25:57 +0100

    Created Category object, Styling and many-many associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: {maximum: 19, minimum: 5}
   validates_uniqueness_of :name
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
 end
